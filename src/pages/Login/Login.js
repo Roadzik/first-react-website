ec6da36e0681b0b91805e8bacb1c9f1e3a9cccb4
Roadzik <roadzik66@gmail.com>
2024-01-24 01:35:04 +0100

    Added login validation and fetch request

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,43 +1,61 @@
-import { useState } from 'react'
-import './Login.css'
+import { useState } from "react";
+import "./Login.css";
 const Login = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [error ,setError] = useState('')
-    const handleUserInput = () =>{
-        if(username === '' || username==null || password === '' || password==null) return 0
-        return [setError('')]
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if(!handleUserInput()) return setError('Please fill the data')
-        fetch('http://localhost:4000/api/login', {
-            method:'POST',
-            headers:{
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({username, password})
-        }).then(()=>{
-            console.log('data sent')
-        }).catch((err)=>{
-            console.log(err)
-        })
-    }
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const handleUserInput = () => {
+		if (
+			username === "" ||
+			username == null ||
+			password === "" ||
+			password == null
+		)
+			return 0;
+		return [setError("")];
+	};
+	async function handleSubmit(e) {
+		e.preventDefault();
+		if (!handleUserInput()) return setError("Please fill the data");
 
-    return (
-        <div className="container">
-            <div className='form-container'>
-                <div className="error">{error}</div>
-                <form onSubmit={e=>handleSubmit(e)} method='POST'>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" onInput={e=>setUsername(e.target.value)}/>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" onInput={e=>setPassword(e.target.value)}/>
-                    <input type="submit" value="Login" />
-                </form>
-            </div>
-        </div>
-    );
-}
- 
+		let response = await fetch("http://localhost:4000/api/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ username, password }),
+		});
+
+		return response.json();
+	}
+
+	return (
+		<div className='login-container'>
+			<div className='form-container'>
+				<div className='error'>{error}</div>
+				<form
+					onSubmit={(e) => handleSubmit(e).then((data) => console.log(data))}
+					method='POST'
+				>
+					<label htmlFor='username'>Username</label>
+					<input
+						type='text'
+						name='username'
+						id='username'
+						onInput={(e) => setUsername(e.target.value)}
+					/>
+					<label htmlFor='password'>Password</label>
+					<input
+						type='password'
+						name='password'
+						id='password'
+						onInput={(e) => setPassword(e.target.value)}
+					/>
+					<input type='submit' value='Login' />
+				</form>
+			</div>
+		</div>
+	);
+};
+
 export default Login;
