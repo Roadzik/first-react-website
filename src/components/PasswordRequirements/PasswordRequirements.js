4bd711efa3fa00122089d8af730446bf2ab5c84b
Roadzik <roadzik66@gmail.com>
2024-01-24 01:38:15 +0100

    Added a Component for Handling Password Requirements

---
+++ src/components/PasswordRequirements/PasswordRequirements.js
@@ -0,0 +1,31 @@
+import "./PasswordRequirements.css";
+const PasswordRequirements = (props) => {
+	let password = props.password;
+	return (
+		<div className='password-requirements'>
+			<h4>Password has to contain</h4>
+			{password.length >= 8 ? (
+				<p style={{ color: "green" }}>At least 8 characters</p>
+			) : (
+				<p>At least 8 characters</p>
+			)}
+			{/[A-Z]/.test(password) ? (
+				<p style={{ color: "green" }}>At least 1 Upper Case character</p>
+			) : (
+				<p>At least 1 Upper Case character</p>
+			)}
+			{/[a-z]/.test(password) ? (
+				<p style={{ color: "green" }}>At least 1 Lower Case character</p>
+			) : (
+				<p>At least 1 Lower Case character</p>
+			)}
+			{/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/.test(password) ? (
+				<p style={{ color: "green" }}>At least 1 special character</p>
+			) : (
+				<p>At least 1 special character</p>
+			)}
+		</div>
+	);
+};
+
+export default PasswordRequirements;
